0f84a6dc165a5ebeb77c663fb7789e4d0558c63d
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-11-07 15:41:48 +0200

    Buttons & Socials

--- scr/js/main.js
+++ scr/js/main.js
@@ -15,3 +15,28 @@ setInterval(updateTime, 1000);
 
 // Викликати функцію один раз для миттєвого показу часу
 updateTime();
+
+
+document.getElementById('btn-portfolio').addEventListener('click', function() {
+    document.getElementById('projects').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('btn-work-with-me').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('btn-work-with-me-2').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('btn-send-email').addEventListener('click', function () {
+    // Додайте необхідну дію для кнопки "Send Email"
+    window.location.href = 'mailto:your_email@example.com';
+});
+
